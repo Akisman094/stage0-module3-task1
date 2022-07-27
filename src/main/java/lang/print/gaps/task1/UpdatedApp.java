dce55bd224b49990804835a6f2b50bf2a583dddc
Akmal Ismoilov <master80098@gmail.com>
2022-07-27 15:01:50 +0500

    Fixed everything!

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -1,7 +1,8 @@
 package lang.print.gaps.task1;
 
 public class UpdatedApp {
-    public static void main(String[] args) {
-        System.out.println("fixMe");
+    public static void main(String[] args)
+    {
+        System.out.println("I'm\nupdated\ncan\nbe\ncompiled.\nAnd\ncan\nwrite\nto\nconsole");
     }
 }
